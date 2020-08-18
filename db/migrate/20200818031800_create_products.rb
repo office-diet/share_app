bf272ded032d5a5a702dc8987a7e00218d3d52ad
office-diet <kw278u@bma.biglobe.ne.jp>
2020-08-18 12:23:01 +0900

    productsテーブル生成

--- db/migrate/20200818031800_create_products.rb
+++ db/migrate/20200818031800_create_products.rb
@@ -3,7 +3,7 @@ class CreateProducts < ActiveRecord::Migration[6.0]
     create_table :products do |t|
       t.string :name, null: false
       t.string :image, null: false
-        
+      
       t.timestamps
     end
   end
