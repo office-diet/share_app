e2f64c50e58f4a91848e1b189f93c9e0b792e308
office-diet <kw278u@bma.biglobe.ne.jp>
2020-08-19 21:20:43 +0900

    productsコントローラ仮設定

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,4 +2,8 @@ class ProductsController < ApplicationController
   def index
     @products = Product.all.page(params[:page]).per(20)
   end
+
+  def search
+    @products = Product.where("NAME LIKE '%#{params[:key]}%'").page(params[:page]).per(20)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'products#index'
+  resources :products, only: [:index] do
+    collection do
+      get 'search'
+    end
+  end
 end
